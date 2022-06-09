1423dae254a44da413c6032667cc7a39ee7e3245
br14nzgit <br14ncunningham@gmail.com>
2022-06-09 08:34:07 -0500

    first commit

---
+++ src/main/java/com/rev_app/RevAppApplication.java
@@ -0,0 +1,19 @@
+package com.rev_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class RevAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevAppApplication.class, args);
+	}
+	@GetMapping("/guest")
+	public String guest(){
+		return"Hello Guest";
+	}
+}
---
+++ src/test/java/com/rev_app/RevAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rev_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
